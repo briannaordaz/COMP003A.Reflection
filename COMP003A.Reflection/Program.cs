55d931d20d04ef22fcd2945952b6efec02acbe39
briannaordaz <briannaordaz5@gmail.com>
2025-02-26 20:44:19 -0800

    added a variable to store user's input

--- COMP003A.Reflection/Program.cs
+++ COMP003A.Reflection/Program.cs
@@ -5,17 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("1. What worked well for you over the last 8 weeks? ");
+        string Q1 = Console.ReadLine();
         
         Console.WriteLine("2. What external resources did you use over the last 8 weeks? ");
+        string Q2 = Console.ReadLine();
         
         Console.WriteLine("3. What would you change about the course? ");
+        string Q3 = Console.ReadLine();
         
         Console.WriteLine("4. Did you learn what you expected to learn in this course? ");
+        string Q4 = Console.ReadLine();
         
         Console.WriteLine("5. What Concepts were new to you? ");
+        string Q5 = Console.ReadLine();
         
         Console.WriteLine("6. What concepts have you encountered already in the past? ");
+        string Q6 = Console.ReadLine();
         
         Console.WriteLine("7. Do you have any tips/comments for the instructor? ");
+        string Q7 = Console.ReadLine();
     }
 }
